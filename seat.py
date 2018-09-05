b25288fb94b9dde84606de97f71e7a75832fd416
Pedro H <pedrohmpereira@gmail.com>
2018-09-05 11:11:38 -0300

    Primeiro milestone
    Faz get e post dos endereços pedidos e salva a resposta.
    
    Ordena os tickets os tickets e remove duplicatas.

---
+++ seat.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python2
+import requests
+
+
+def get_emissao(ticket):
+    return int(ticket["emissao"])
+
+
+def print_tickets(tickets):
+    for t in tickets:
+        print t["senha"],
+        print t["emissao"],
+        print t["prioridade"]
+
+
+def get_unique(tickets):
+    uniq = list([])
+
+    for t in tickets:
+        if t in uniq:
+            continue
+        else:
+            uniq.append(t)
+        print t["senha"]
+    return uniq
+
+
+def main():
+    name = "Pedro Henrique"
+    url = "http://seat.ind.br/processo-seletivo/2018/02/desafio.php"
+    payload = {"nome": name}
+
+    rGet = requests.get(url, payload)
+    data = rGet.json()
+
+    tickets = data["input"]
+    uniq = get_unique(tickets)
+
+    uniq.sort(key=get_emissao)
+
+    result = {"nome": name, "chave": data["chave"], "resultado": uniq}
+    rPost = requests.post(data["postTo"]["url"], data=result)
+
+    with open("output.html", "w") as f:
+        f.write(rPost.content)
+
+
+if __name__ == "__main__":
+    main()
