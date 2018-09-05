3020ce1afda2e8734ecba30b7d64bd104cdbd87e
Pedro H <pedrohmpereira@gmail.com>
2018-09-05 13:12:51 -0300

    Ajustes e clean up no código
    Remove função desnecessária, para ficar mais limpo o código. Move main
    para o inicio para maior visibilidade.

--- seat.py
+++ seat.py
@@ -2,10 +2,35 @@
 import requests
 
 
+def main():
+    name = "Pedro Henrique"
+    url = "http://seat.ind.br/processo-seletivo/2018/02/desafio.php"
+    payload = {"nome": name}
+
+    rGet = requests.get(url, payload)
+    data = rGet.json()
+    tickets = data["input"]
+
+    uniq = get_unique(tickets)
+    uniq.sort(key=get_emissao)
+    uniq.sort(key=get_prioridade)
+
+    fill_stats(uniq)
+    print_hist(uniq)
+
+    result = {"nome": name, "chave": data["chave"], "resultado": uniq}
+    rPost = requests.post(data["postTo"]["url"], data=result)
+
+    with open("output.html", "w") as f:
+        f.write(rPost.content)
+
+
+# Get usado para ordenacao
 def get_emissao(ticket):
     return int(ticket["emissao"])
 
 
+# Calcula prioridade para ordenacao, foi utilizado -1 para tickets ja atendidos
 def get_prioridade(ticket):
     priorityList = {"atendido": -1, "preferencial": 0, "geral": 1}
     priority = priorityList[ticket["prioridade"]]
@@ -15,6 +40,7 @@ def get_prioridade(ticket):
     return int(priority)
 
 
+# Preenche os campos novos pedidos "naFrente" e "espera"
 def fill_stats(tickets):
     position = 0
     waitTime = avg_service_time(tickets)
@@ -28,13 +54,7 @@ def fill_stats(tickets):
         position += 1
 
 
-def print_tickets(tickets):
-    for t in tickets:
-        print t["senha"],
-        print t["emissao"],
-        print t["prioridade"]
-
-
+# Calcula o tempo medio de atendimento com base nos atendimentos anteriores
 def avg_service_time(tickets):
     serviceTime = list([])
     for t in tickets:
@@ -44,18 +64,17 @@ def avg_service_time(tickets):
     return sum(serviceTime)/len(serviceTime)
 
 
+# Preenche uma nova lista com elementos unicos das lista de tickets
 def get_unique(tickets):
     uniq = list([])
 
     for t in tickets:
-        if t in uniq:
-            continue
-        else:
+        if t not in uniq:
             uniq.append(t)
-        print t["senha"]
     return uniq
 
 
+# Imprime o histograma no formato especificado
 def print_hist(tickets):
     waitTimes = []
     for t in tickets:
@@ -71,29 +90,6 @@ def print_hist(tickets):
             "< 15min: {}{}{}", "> 15min: {}{}{}"]
     for i in range(4):
         print text[i].format("#"*hist[i], ' ' if hist[i] > 0 else '', hist[i])
-
-
-def main():
-    name = "Pedro Henrique"
-    url = "http://seat.ind.br/processo-seletivo/2018/02/desafio.php"
-    payload = {"nome": name}
-
-    rGet = requests.get(url, payload)
-    data = rGet.json()
-
-    tickets = data["input"]
-    uniq = get_unique(tickets)
-
-    uniq.sort(key=get_emissao)
-    uniq.sort(key=get_prioridade)
-    fill_stats(uniq)
-    print_hist(uniq)
-
-    result = {"nome": name, "chave": data["chave"], "resultado": uniq}
-    rPost = requests.post(data["postTo"]["url"], data=result)
-
-    with open("output.html", "w") as f:
-        f.write(rPost.content)
 
 
 if __name__ == "__main__":
